158217c015957ec0d18b25e6f221bb932f802c64
Benjamin Smith <bh2smith@users.noreply.github.com>
2025-07-21 16:34:18 +0200

    Update Subscription Module Contract (#4)
    Preparing for contract upgrade see:
    
    https://github.com/deluXtreme/subi-contracts

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,19 @@ mod redeem;
 
 use alloy::signers::local::PrivateKeySigner;
 use std::env;
+use tracing_subscriber::FmtSubscriber;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv::dotenv().ok();
+    // Initialize the tracing subscriber
+    let subscriber = FmtSubscriber::builder()
+        // TODO: Change to DEBUG! https://github.com/deluXtreme/redeem-rs/issues/6
+        .with_max_level(tracing::Level::INFO)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("Failed to set tracing subscriber");
     let subscriptions = fetch::fetch_redeemable_subscriptions().await?;
-    println!("Found {} subscriptions", subscriptions.len());
+    tracing::info!("Found {} subscriptions", subscriptions.len());
     let signer: PrivateKeySigner = env::var("PK")?.parse()?;
     for subscription in subscriptions {
         redeem::redeem_payment(signer.clone(), subscription).await?;
--- src/redeem.rs
+++ src/redeem.rs
@@ -1,4 +1,3 @@
-use crate::redeem::TypeDefinitions::{FlowEdge, Stream};
 use alloy::{
     primitives::{Address, U256, aliases::U192},
     providers::ProviderBuilder,
@@ -7,98 +6,87 @@ use alloy::{
 };
 use serde::{Deserialize, Serialize};
 
-use circles_pathfinder::{FindPathParams, prepare_flow_for_contract};
+use circles_pathfinder::{FindPathParams, encode_redeem_trusted_data, prepare_flow_for_contract};
 use std::str::FromStr;
 
 sol!(
     #[allow(missing_docs)]
     #[sol(rpc)]
-    Hub,
+    SubscriptionModule,
     "src/redeem.json"
 );
 
+const GNOSIS_RPC: &str = "https://rpc.gnosischain.com/";
 const CIRCLES_RPC: &str = "https://rpc.aboutcircles.com/";
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "lowercase")]
+pub enum Category {
+    Trusted,
+    Untrusted,
+    Group,
+}
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct RedeemableSubscription {
+    pub id: String,
     pub recipient: String,
     pub subscriber: String,
     pub amount: String,
-    pub module: String,
-    pub sub_id: String,
+    pub periods: i32,
+    pub category: Category,
 }
 
 pub async fn redeem_payment(
     signer: PrivateKeySigner,
     subscription: RedeemableSubscription,
 ) -> Result<bool, Box<dyn std::error::Error>> {
-    let subscription_manager = "0x7E9BaF7CC7cD83bACeFB9B2D5c5124C0F9c30834".parse::<Address>()?;
+    let subscription_module = "0xcEbE4B6d50Ce877A9689ce4516Fe96911e099A78"
+        .parse::<Address>()
+        .unwrap();
 
     let provider = ProviderBuilder::new()
         .wallet(signer)
-        .connect_http(CIRCLES_RPC.parse()?);
+        .connect_http(GNOSIS_RPC.parse()?);
+    let contract = SubscriptionModule::new(subscription_module, provider);
+    let id = U256::from_str(&subscription.id)?;
+    let tx;
+    tracing::info!("Redeeming {:#?}", subscription);
+    if subscription.category != Category::Trusted {
+        tx = contract.redeem(id.into(), vec![].into()).send().await?;
+    } else {
+        let amount = U192::from_str(&subscription.amount)?;
+        let periods = U192::from(subscription.periods as u64);
+        let params = FindPathParams {
+            from: subscription.subscriber.parse::<Address>()?,
+            to: subscription.recipient.parse::<Address>()?,
+            target_flow: amount * periods,
+            use_wrapped_balances: Some(true),
+            from_tokens: None,
+            to_tokens: None,
+            exclude_from_tokens: None,
+            exclude_to_tokens: None,
+        };
 
-    let params = FindPathParams {
-        from: subscription.subscriber.parse::<Address>()?,
-        to: subscription.recipient.parse::<Address>()?,
-        target_flow: U192::from_str(&subscription.amount)?,
-        use_wrapped_balances: Some(true),
-        from_tokens: None,
-        to_tokens: None,
-        exclude_from_tokens: None,
-        exclude_to_tokens: None,
-    };
+        // This automatically:
+        // - Finds the optimal path
+        // - Creates the flow matrix
+        // - Converts to contract-compatible types
+        // - Handles flow balancing
+        let path_data = prepare_flow_for_contract(CIRCLES_RPC, params).await?;
+        let data = encode_redeem_trusted_data(
+            path_data.flow_vertices,
+            path_data.flow_edges,
+            path_data.streams,
+            path_data.packed_coordinates,
+            path_data.source_coordinate,
+        );
+        tx = contract.redeem(id.into(), data.into()).send().await?;
+    }
 
-    // This automatically:
-    // - Finds the optimal path
-    // - Creates the flow matrix
-    // - Converts to contract-compatible types
-    // - Handles flow balancing
-    let path_data = prepare_flow_for_contract(CIRCLES_RPC, params).await?;
-
-    // Convert pathfinder types to contract-specific types
-    // Types are exactly the same but because they live in different modules
-    // Rust treats them as different. Still have to do the conversion :(
-    let contract_flow_edges: Vec<FlowEdge> = path_data
-        .to_flow_edges()
-        .into_iter()
-        .map(|edge| FlowEdge {
-            streamSinkId: edge.streamSinkId,
-            amount: edge.amount,
-        })
-        .collect();
-
-    let contract_streams = path_data
-        .to_streams()
-        .into_iter()
-        .map(|stream| Stream {
-            sourceCoordinate: stream.sourceCoordinate,
-            flowEdgeIds: stream.flowEdgeIds,
-            data: stream.data,
-        })
-        .collect();
-
-    let module = subscription.module.parse::<Address>()?;
-    let sub_id = U256::from_str(&subscription.sub_id)?;
-
-    let contract = Hub::new(subscription_manager, provider);
-
-    let tx = contract
-        .redeemPayment(
-            module,
-            sub_id,
-            path_data.clone().flow_vertices,
-            contract_flow_edges,
-            contract_streams,
-            path_data.to_packed_coordinates(),
-        )
-        .send()
-        .await?;
-
-    println!(
-        "Redeemed {}-{} at: {}",
-        subscription.sub_id,
-        subscription.module,
+    tracing::info!(
+        "Redeemed {} at: https://gnosisscan.io/tx/{}",
+        subscription.id,
         tx.tx_hash()
     );
 
